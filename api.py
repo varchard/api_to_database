83a133123d9089b4625d4c49247f8222f79d8ace
varchard <106408534+varchard@users.noreply.github.com>
2022-07-14 14:12:46 -0400

    cleans the data a little better before export. makes units consistent, drops extra columns

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import datetime
 import psycopg2 
 from sqlalchemy import create_engine
 import os
+from re import compile
 
 # vars
 db_host = os.environ.get('db_host')
@@ -22,7 +23,45 @@ df = pd.DataFrame(dataset) #converts dataset to pandas dataframe
 
 # clean
 df.dropna(axis=1, how='all', inplace=True) # drops unused columns
+df.drop(columns = ['strVideo','strIBA','strInstructionsES', 'strInstructionsDE', 'strInstructionsIT', 
+    'strDrinkThumb','strImageSource', 'strImageAttribution'], inplace = True)
 df['timestamp'] = datetime.datetime.now() # appends timestamp of when data was collected
+
+def unit_scrub(value):
+    '''accepts value from column with multiple units of measure and returns measure in Oz'''
+    num_search = compile('\d*\.\d*|\d')
+    cl_search = compile('cl')
+    part_search = compile('part')
+    shot_search = compile('shot|Shot')
+    try:
+        value = value.replace('1/8', '.125')
+        value = value.replace('1/4', '.25')
+        value = value.replace(' 1/2', '.5')
+        value = value.replace('1/2','.5')
+        value = value.replace(' 3/4', '.75')
+        value = value.replace('1/3', '.333')
+    except AttributeError:
+        return None
+    if cl_search.search(value):
+        num = round((float((num_search.findall(value))[0]) * 0.33814),2)
+        cleaned = str(num) + ' oz'
+        return cleaned
+    elif part_search.search(value):
+        num = round((float(num_search.findall(value)[0]) / 2), 2)
+        cleaned = str(num) + ' oz'
+        return cleaned
+    elif shot_search.search(value):
+        num = round((float(num_search.findall(value)[0]) * 1.5), 2)
+        cleaned = str(num) + ' oz'
+        return cleaned
+    else:
+        return value
+
+cols_to_clean = ['strMeasure1', 'strMeasure2', 'strMeasure3', 'strMeasure4', 'strMeasure5', 'strMeasure6']
+for col in cols_to_clean:
+    df[col] = df[col].map(unit_scrub)
+
+# df.to_clipboard()
 
 #send to postgres server
 conn = psycopg2.connect(dbname= db_name, user = db_user, password = db_pass, host = db_host) # sets connection to postgres database
